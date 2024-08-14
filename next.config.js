b5e9b29372f38cb61af27e510bdc729616881782
Ken Abraham <kenmabrahamm@gmail.com>
2024-08-13 21:20:01 -0400

    changes

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: true,
+  // other configs...
+}
+
+console.log('Environment variables:', {
+  NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
+  // Log other non-sensitive variables here
+});
 
 module.exports = nextConfig
